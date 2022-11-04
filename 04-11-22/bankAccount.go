9578a0a112f9a1485c9624247e456ce0c85307aa
karthikbanjan <banjankarthik@gmail.com>
2022-11-04 11:30:22 +0530

    1

--- 04-11-22/bankAccount.go
+++ 04-11-22/bankAccount.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+type BankAccount struct {
+	accountNo int
+	owner     string
+	balance   float64
+}
+
+func main() {
+	bankAccount1 := BankAccount{accountNo: 1234, owner: "John Doe", balance: 1000.0}
+	bankAccount2 := BankAccount{accountNo: 5678, owner: "Jane Doe", balance: 2000.0}
+
+	an := 0
+	deposit := 0
+	fmt.Println("Enter account number: ")
+	fmt.Scan(&an)
+	fmt.Println("Enter deposit amount: ")
+	fmt.Scan(&deposit)
+
+	if bankAccount1.accountNo == an {
+		bankAccount1.balance += float64(deposit)
+		fmt.Println("New balance: ", bankAccount1.balance)
+	} else if bankAccount2.accountNo == an {
+		bankAccount2.balance += float64(deposit)
+		fmt.Println("New balance: ", bankAccount2.balance)
+	} else {
+		fmt.Println("Invalid account number")
+	}
+
+	fmt.Println(bankAccount1)
+	fmt.Println(bankAccount2)
 }
