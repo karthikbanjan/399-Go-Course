66adc3a1d7dc6dd49d8729a1390aca56567174b0
karthikbanjan <banjankarthik@gmail.com>
2022-11-18 17:20:06 +0530

    Completed Q11

---
+++ Assignment 1/Q11_FibonacciNum_CSE0238.go
@@ -0,0 +1,31 @@
+/*
+11. The Fibonacci sequence is defined as: fib (0) = 0, fib (1) = 1,
+fib(n) = fib(n-1) + fib(n-2). Write a recursive function in go which can find fib(n).
+*/
+
+package main
+
+import "fmt"
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+
+	if n == 1 {
+		return 1
+	}
+
+	return fib(n-1) + fib(n-2)
+}
+
+func main() {
+	var n int
+
+	for {
+		fmt.Println("\nEnter position of fibonacci number(Starts from 0):")
+		fmt.Scan(&n)
+
+		fmt.Println("Fibonacci number at position", n, "is", fib(n))
+	}
+}
