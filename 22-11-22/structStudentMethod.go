0bf8056da26487ed31be9656ea1e1000a9cc4d6e
karthikbanjan <banjankarthik@gmail.com>
2022-11-22 14:32:18 +0530

    22-11-22 Methods

---
+++ 22-11-22/structStudentMethod.go
@@ -0,0 +1,26 @@
+package main
+
+type student struct {
+	name  string
+	age   int
+	marks [5]int
+}
+
+func (s student) averageMarks() int {
+	sum := 0
+	for _, v := range s.marks {
+		sum += v
+	}
+	return sum / len(s.marks)
+}
+
+func (s student) studentDetails() {
+	println("Name:", s.name)
+	println("Age:", s.age)
+	println("Average Marks:", s.averageMarks())
+}
+
+func main() {
+	s := student{"Karthik Banjan", 21, [5]int{99, 98, 97, 96, 95}}
+	s.studentDetails()
+}
